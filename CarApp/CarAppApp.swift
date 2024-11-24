1ab473a549d1172846240adfc2f8490203c7361b
Vagarth <vagarth419@gmail.com>
2024-11-24 21:42:04 +0530

    Initial Commit

---
+++ CarApp/CarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAppApp.swift
+//  CarApp
+//
+//  Created by Vagarth Pandey on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarApp
+//
+//  Created by Vagarth Pandey on 24/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
